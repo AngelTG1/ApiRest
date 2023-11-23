50897b579420c1f42ddb360d135987bbefe5bf35
AngelTg <angeltgz30@gmail.com>
2023-11-22 19:08:47 -0600

    usuarios

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,93 +1,67 @@
-  import { pool } from "../db.js";
+import { pool } from "../db.js";
 
-  export const getUsuarios = async (req, res) => {
-    try {
-      const [rows] = await pool.query("SELECT * FROM usuarios");
-      res.json(rows);
-    } catch (error) {
-      return res.status(500).json({ message: "Something goes wrong" });
+export const getUsuarios = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM usuarios");
+    res.json(rows);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const getUsuario = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+
+    if (rows.length <= 0) {
+      return res.status(404).json({ message: "Usuario not found" });
     }
-  };
 
-  export const getUsuario = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+    res.json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
-      if (rows.length <= 0) {
-        return res.status(404).json({ message: "Usuario not found" });
-      }
+export const deleteUsuario = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [result] = await pool.query("DELETE FROM usuarios WHERE id = ?", [id]);
 
-      res.json(rows[0]);
-    } catch (error) {
-      return res.status(500).json({ message: "Something goes wrong" });
+    if (result.affectedRows <= 0) {
+      return res.status(404).json({ message: "Usuario not found" });
     }
-  };
 
-  export const deleteUsuario = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const [rows] = await pool.query("DELETE FROM usuarios WHERE id = ?", [id]);
+    res.sendStatus(204);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
-      if (rows.affectedRows <= 0) {
-        return res.status(404).json({ message: "Usuario not found" });  
-      }
+export const createUsuario = async (req, res) => {
+  try {
+    const { nombre, apellido, correo, contrasenia, rol } = req.body;
 
-      res.sendStatus(204);
-    } catch (error) {
-      return res.status(500).json({ message: "Something goes wrong" });
+    if (!nombre || !apellido || !correo || !contrasenia || !rol) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-  };
 
-  // export const createUsuario = async (req, res) => {
-  //   try {
-  //     const { nombre, apellido, correo, contrasenia, rol } = req.body;
-  //     const [rows] = await pool.query(
-  //       "INSERT INTO usuarios (nombre, apellido, correo, contrasenia, rol) VALUES (?, ?, ?, ?, ?)",
-  //       [nombre, apellido, correo, contrasenia, rol]
-  //     );
-  //     res.status(201).json({ id: rows.insertId, nombre, apellido, correo, contrasenia, rol });
-  //   } catch (error) {
-  //     return res.status(500).json({ message: "Something goes wrong" });
-  //   }
-  // };
+    const [userResult] = await pool.query(
+      "INSERT INTO usuarios (nombre, apellido, correo, contrasenia, rol) VALUES (?, ?, ?, ?, ?)",
+      [nombre, apellido, correo, contrasenia, rol]
+    );
 
-  export const createUsuario = async (req, res) => {
-    try {
-      const { nombre, apellido, correo, contrasenia, rol } = req.body;
-      const [userResult] = await pool.query(
-        "INSERT INTO usuarios (nombre, apellido, correo, contrasenia, rol) VALUES (?, ?, ?, ?, ?)",
-        [nombre, apellido, correo, contrasenia, rol]
-      );
-  
-      const usuario_id = userResult.insertId;
-  
-      // Crea un carrito vacío vinculado al nuevo usuario
-      await pool.query("INSERT INTO carritos (usuario_id) VALUES (?)", [usuario_id]);
-  
-      res.status(201).json({ id: usuario_id, nombre, apellido, correo, contrasenia, rol });
-    } catch (error) {
-      return res.status(500).json({ message: "Something goes wrong" });
-    }
-  };
+    const usuario_id = userResult.insertId;
 
-  export const updateUsuario = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { nombre, apellido, correo, contrasenia, rol } = req.body;
+    res.status(201).json({ id: usuario_id, nombre, apellido, correo, contrasenia, rol });
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
-      const query = "UPDATE usuarios SET nombre = IFNULL(?, nombre), apellido = IFNULL(?, apellido), correo = IFNULL(?, correo), contrasenia = IFNULL(?, contrasenia), rol = IFNULL(?, rol) WHERE id = ?";
-      const values = [nombre, apellido, correo, contrasenia, rol, id];
 
-      const [result] = await pool.query(query, values);
-
-      if (result.affectedRows === 0)
-        return res.status(404).json({ message: "Usuario not found" });
-
-      const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
-
-      res.json(rows[0]);
-    } catch (error) {
-      return res.status(500).json({ message: "Something goes wrong" });
-    }
-  };
